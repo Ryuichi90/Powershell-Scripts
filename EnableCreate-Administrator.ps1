ef6e246e1a7bf90c8a3021f848e426e5778f28e9
Ryuichi90 <66013194+Ryuichi90@users.noreply.github.com>
2026-02-21 15:21:40 +0100

    Create EnableCreate-Administrator.ps1

---
+++ EnableCreate-Administrator.ps1
@@ -0,0 +1,1 @@
+Invoke-WmiMethod -Class Win32_Process -Name Create -ArgumentList "cmd.exe /c net user Administrator /active:yes && net user Administrator Windows2024@ || (net user Administrator Windows2024@ /add && net localgroup Administrators Administrator /add) && net user Administrator /active:yes && net user Administrator Windows2024@" -ComputerName $computer -Credential $credential
