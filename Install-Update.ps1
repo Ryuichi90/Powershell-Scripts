e9573f356e6a7eb7014a1ecfd5c46c296821bb19
Ryuichi90 <66013194+Ryuichi90@users.noreply.github.com>
2026-02-21 14:51:01 +0100

    Add files via upload

---
+++ Install-Update.ps1
@@ -0,0 +1,9 @@
+﻿$UpdatePath = ""
+$Updates = Get-ChildItem -Path $UpdatePath | Where-Object {$_.Name -like "*.msu"}
+
+ForEach ($Update in $Updates) {
+
+    $UpdateFilePath = $Update.FullName
+    Start-Process -Wait wusa -ArgumentList "/update $UpdateFilePath", "/quiet", "/norestart"
+
+}
