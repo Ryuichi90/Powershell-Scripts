5f1d8d6968b519f7af65011eb5b758925d708aad
Ryuichi90 <66013194+Ryuichi90@users.noreply.github.com>
2026-02-21 16:13:34 +0100

    Add files via upload

---
+++ Export-HWID.ps1
@@ -0,0 +1,9 @@
+﻿Install-PackageProvider -Name NuGet -Force -Confirm:$False
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$serialNumber = (Get-WmiObject win32_bios | select -ExpandProperty Serialnumber)
+$fileName = $serialNumber + "_AutopilotHWID.csv"
+Set-Location -Path "HWID"
+$env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+Install-Script -Name Get-WindowsAutopilotInfo -Force -Confirm:$False
+Get-WindowsAutopilotInfo -OutputFile $fileName -GroupTag "Autopilot-Provisioning"
