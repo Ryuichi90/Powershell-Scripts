29dfcae93d1a6da2f62fa0575121c71246a86a71
Ryuichi90 <66013194+Ryuichi90@users.noreply.github.com>
2026-02-21 14:57:38 +0100

    Add files via upload

---
+++ Start-HyperVVMsRemote.ps1
@@ -0,0 +1,27 @@
+﻿$vmNames = @("HYD-DC1", "HYD-CM1", "HYD-CLIENT1")
+$remoteMachine = "RemotePC"
+
+$securePasswordforUser = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ("Administrator", $securePasswordforUser)
+
+foreach ($vm in $vmNames) {
+    $result = Invoke-Command -ComputerName $remoteMachine -Credential $credential -ScriptBlock {
+        param($vmName)
+        try {
+            $vmState = (Get-VM -Name $vmName).State
+            if ($vmState -eq "Running") {
+                "$vmName is already running."
+            } else {
+                Start-VM -Name $vmName -ErrorAction Stop
+				"$vmName started successfully."
+                
+            }
+        } catch {
+            "Failed to start $vmName - $_"
+        }
+    } -ArgumentList $vm
+
+    Write-Host $result
+}
+Write-Host "The VMs have been started"
+Read-Host
