4d6f01db09daafc3513080672c1c6e56f3cc6701
Ryuichi90 <66013194+Ryuichi90@users.noreply.github.com>
2026-02-21 14:54:11 +0100

    Add files via upload

---
+++ Get-BatteryInfos.ps1
@@ -0,0 +1,8 @@
+﻿$designedCapacity = (Get-WmiObject -Class BatteryStaticData -Namespace ROOT\WMI).DesignedCapacity
+$fullChargedCapacity = (Get-WmiObject -Class BatteryFullChargedCapacity -Namespace ROOT\WMI).FullChargedCapacity
+
+Write-Host "--------------------------------------------" -ForegroundColor Cyan
+Write-Host "Battery informations:" -ForegroundColor Cyan
+Write-Host "--------------------------------------------" -ForegroundColor Cyan
+Write-Host "DesignedCapacity: " -ForegroundColor Yellow -NoNewline; $designedCapacity
+Write-Host "FullChargedCapacity: " -ForegroundColor Yellow -NoNewline; $fullChargedCapacity
